a68e2ed4d95eff2eadaa110316e49d587d4f3e62
taylorpleva16 <24200230-taylorpleva16@users.noreply.replit.com>
2023-10-30 14:14:11 +0000

    10/30

--- js/application.js
+++ js/application.js
@@ -1,33 +1,49 @@
+$(document).ready(function() {
 
+  $(document).on('click','.btn-remove', function(){
+    $(this).parent().parent().remove();
+  });
+  $(document).on('click', '.btn-add', function(){
+    var item = $('.newItemName input').val();
+    var cost = $('.newItemPrice input').val();
+    var html = '<tr><td class="itemName">' + item + '</td><td class="itemPrice">$' + cost + '.00</td><td class="quantity">QTY <input type="number" value="0"></td class="itemTotal"><td>$ </td><td><button class="btn btn-remove">Remove</button></td></tr>';
+    if (cost == '' || item == ''){
+      alert('Please enter a valid item name and price');
+    }
+    else {
+      $('.items').append(html);
+      $('.newItemName input').val('');
+      $('.newItemPrice input').val('');
+    };
+  });
 
-$(document).on('click','.btn-remove', function(){
-  $(this).parent().parent().remove();
-});
-$(document).on('click', '.btn-add', function(){
-  var item = $('#newItemName input').val();
-  var cost = $('#newItemPrice input').val();
-  var html = '<tr><td>' + item + '</td><td>$' + cost + '.00</td><td>QTY <input type="number" value="0"></td><td>$ </td><td><button class="btn btn-remove">Remove</button></td></tr>';
-  if (cost == '' || item == ''){
-    alert('Please enter a valid item name and price');
-  }
-  else {
-    $('#items').append(html);
-    $('#newItemName input').val('');
-    $('#newItemPrice input').val('');
+  function sum() {
+    $('.items tr').each(function(){
+      var total = 0;
+      var price = Number(($(this).find('.itemPrice').html()).replace(/[^0-9\.]+/g, ""));
+      var quantity = $(this).find('.quantity input').val();
+      var subtotal = price*quantity;
+
+      //Update item price to value or $0.00
+      if (subtotal != 0) {
+        $(this).find('.itemTotal').text("$" + subtotal + ".00");
+      } else {
+        $(this).find('.itemTotal').text("$0.00");
+      }
+
+      //Calculate cart total based on subtotals
+      total += subtotal;
+      $('.cartTotal').text("Cart Total: $" + total + ".00");
+      return total;
+    })
   };
+  
+  sum();
+  $(document).on('keyup', '.quantity', function () {
+    sum();
+  });
+  $(document).on('click', '.quantity', function() {
+    sum();
+  });
 });
-
-function subTotal() {
-  var subTotal = 0;
-  $('tr').each(function(){
-    var cost = $(this).children('#itemPrice').text();
-    var qty = $(this).children('#quantity').children('input').val();
-    var total = qty * cost;
-    subTotal += total;
-    $(this).children('#itemTotal').text('$' + subTotal);
-})
-};
-
-
-
 
--- script.js
+++
@@ -1,3 +0,0 @@
-$(document).on('click','.btn-remove', function(){
-  $(this).parent().remove();
-});
